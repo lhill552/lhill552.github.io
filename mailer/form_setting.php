b4dc451a308b918d3dac10ecc942887aff322da7
Ben Hall <benmhall1998@gmail.com>
2019-02-18 12:29:14 +0000

    initial commit of website template

---
+++ mailer/form_setting.php
@@ -0,0 +1,8 @@
+<?php
+	/*Form settings*/
+	$subj = "New message from your CV"; //letter subject
+	$to = 'Li-TailorHill@outlook.com'; // Enter Your E-mail
+	$from = 'contact@litailorhill.me'; // Admin e-mail
+	$fromName = 'CV Contact Form'; // Your company name
+	$charset = 'UTF-8';
+?>
